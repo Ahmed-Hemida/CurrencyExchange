17eff01080f5eb5d69d4a64489c93395b49ae7c3
Ahmed Hemida <Ahmed-Hemida>
2022-06-09 17:38:30 +0200

    Add project files.

---
+++ CurrencyExchange/CurrencyExchange.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyExchange
+{
+    // *****************************************
+    // DON"T CHANGE CLASS NAME OR FUNCTION NAME
+    // *****************************************
+    public static class CurrencyExchange
+    {
+        //Your Code is Here:
+        //==================
+        /// <summary>
+        /// find the index of the USD Dollar $ with the smallest number of questions.
+        /// </summary>
+        /// <param name="N">Number of customers (N)</param>
+        /// <param name="M">Number of currencies (M)</param>
+        /// <param name="knows">N*M Matrix indicating whether customer i knows currency j or not</param>
+        /// <returns>index of US Dollar</returns>
+        public static int CheckUSD(int N, int M, bool[,] knows)
+        {
+            int row=0;int column=0;while(row<N)if(!knows[row, column])column++;else{row++;}return(column);
+        }
+       
+
+
+    }
+}
